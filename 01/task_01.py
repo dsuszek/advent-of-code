97317ff2843a1137117f757b30a8550ef390da53
QQxxx <dominik.suszek@gmail.com>
2023-03-04 14:26:51 +0100

    The solution for task 01 corrected

--- 01/task_01.py
+++ 01/task_01.py
@@ -1,6 +1,8 @@
 final_results = [0] * 1000
 array = []
-i = 0
+iteration = 0 # auxiliary variable
+biggest_values = []
+biggest_value = 0
 
 # Read the file and save input to list
 with open('data.txt', 'r') as file:
@@ -8,19 +10,31 @@ with open('data.txt', 'r') as file:
     for line in file:
         array.append([int(x) for x in line.split()])
 
-# # Define class Elf with one parameter 'caloriesCarried'
-# class Elf:
-#     def __init__(self, caloriesCarried):
-#         self.caloriesCarried = caloriesCarried
-
 # Loop through all records and divide into sub-lists when empty string is found
 for x in array:
     if not x:
-        i += 1
+        iteration += 1
         continue
     else:
-        final_results[i] += x[0]
+        final_results[iteration] += x[0]
 
 
-print(max(final_results))
+print("The maximum number of calories carried by one elf: " + str(max(final_results)))
+
+def get_n_biggest_values_from_list(n, data_list):
+    iteration = 0
+    for iteration in range(n):
+        biggest_value = 0
+        for x in range(len(final_results)):
+            if biggest_value < final_results[x]:
+                biggest_value = final_results[x]
+        biggest_values.append(biggest_value)
+        final_results.remove(biggest_value)
+
+    print("The amount of calories carried by top " + str(n) + " elves: " + str(sum(biggest_values)))
+
+get_n_biggest_values_from_list(3, final_results)
+
+
+print(sorted(final_results))
 
