032bdcfd4bc21cbe25006da18e796031bffc61c4
QQxxx <dominik.suszek@gmail.com>
2023-03-08 18:33:42 +0100

    The solution for task 03

--- 03/task_03.py
+++ 03/task_03.py
@@ -15,18 +15,22 @@ mapping = {}
 keys = range(52)
 values = list(string.ascii_letters)
 
+# Assign number of points for each letter
 for i in keys:
     mapping[i + 1] = values[i]
 
+# Divide each string into two parts: first and second half
 first_half = list(map(lambda x : x[:int(len(x)/2)], rucksacks))
 second_half = list(map(lambda x : x[int(len(x)/2):], rucksacks))
 
+# Find common char in each pairs of substrings
 for i in range(len(first_half)):
     common_chars.append(''.join(set(first_half[i]).intersection(set(second_half[i]))))
 
 def get_keys_from_value(dictionary, value):
     return [k for k, v in dictionary.items() if v == value][0]
 
+# Get number of points from mapping and sum up
 for i in range(len(common_chars)):
     final_value += get_keys_from_value(mapping, common_chars[i])
 
